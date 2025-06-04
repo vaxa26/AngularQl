26b96e10ad5f19f0c8b96e37e292b490f81f098b
Viet Anh <vietanhde@gmail.com>
2025-06-04 11:44:19 +0200

    AppComponents ist auf HomepageComponents und suche leitet zu neuer seite BuchPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'buecher', component: BooksPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
